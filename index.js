488bd796380d66c322a838159e4e82c35159f484
anthony <anthony.troupenat@studylink.fr>
2019-01-30 15:48:25 +0100

    misc

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
 const CURR_DIR = process.cwd();
 const inquirer = require('inquirer');
 const fs = require('fs');
+const fse = require('fs-extra');
+const klawSync = require('klaw-sync');
 
-const CHOICES = fs.readdirSync(`${__dirname}/templates`);
+const CHOICES = fs.readdirSync(`${__dirname}/templates`)
+  .filter((fileName) => /^\w.*/.test(fileName));
+
+let projectName;
+let projectChoice;
+let templatePath;
 
 const QUESTIONS = [
   {
@@ -23,31 +30,69 @@ const QUESTIONS = [
   },
 ];
 
-function createDirectoryContents(templatePath, newProjectPath) {
-  const filesToCreate = fs.readdirSync(templatePath);
+const QUESTIONS_NEXT = [
+  {
+    name: 'project-name',
+    type: 'input',
+    message: 'Please choose an other project name (already exists):',
+    validate(input) {
+      if (/^([A-Za-z\-_\d])+$/.test(input)) return true;
+      return 'Project name may only include letters, numbers, underscores and hashes.';
+    },
+  },
+];
 
-  filesToCreate.forEach((file) => {
-    const origFilePath = `${templatePath}/${file}`;
+const filterModules = (item) => !item.path.includes('node_modules');
 
-    // get stats about the current file
-    const stats = fs.statSync(origFilePath);
+const readFilesAndWrite = (path) => {
+  try {
+    // return all files root path except directories
+    const files = klawSync(path, {
+      traverseAll: true,
+      filter: filterModules,
+      nodir: true,
+    });
+    // change basePath to current directory
+    const newfilesPath = files.map((file) => file.path.replace(templatePath, `${CURR_DIR}/${projectName}`));
 
-    if (stats.isFile()) {
-      const contents = fs.readFileSync(origFilePath, 'utf8');
+    newfilesPath.forEach((file, index) => {
+      const content = fs.readFileSync(files[index].path, 'utf8');
 
-      const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
-      fs.writeFileSync(writePath, contents, 'utf8');
-    }
-  });
-}
+      // Almost the same as writeFileSync (i.e. it overwrites),
+      // except that if the parent directory does not exist, it's created.
+      // file must be a file path (a buffer or a file descriptor is not allowed).
+      // options are what you'd pass to fs.writeFileSync().
+      fse.outputFile(file, content);
+    });
+    console.log(`Done: ${newfilesPath.length} has been created in ${CURR_DIR}/${projectName}`);
+  } catch (er) {
+    console.error(er);
+  }
+};
+
+
+const isDirAlreadyExist = (name, path) => {
+  if (fs.existsSync(`${CURR_DIR}/${name}`)) {
+    askQuestions(QUESTIONS_NEXT);
+  } else {
+    readFilesAndWrite(path);
+  }
+};
+
+const askQuestions = (questions) => {
+  inquirer.prompt(questions)
+    .then((answers) => {
+      projectName = answers['project-name'];
+      isDirAlreadyExist(projectName, templatePath);
+    });
+};
+
 
 inquirer.prompt(QUESTIONS)
   .then((answers) => {
-    const projectChoice = answers['project-choice'];
-    const projectName = answers['project-name'];
-    const templatePath = `${__dirname}/templates/${projectChoice}`;
+    projectName = answers['project-name'];
+    projectChoice = answers['project-choice'];
+    templatePath = `${__dirname}/templates/${projectChoice}`;
 
-    fs.mkdirSync(`${CURR_DIR}/${projectName}`);
-
-    createDirectoryContents(templatePath, projectName);
+    isDirAlreadyExist(projectName, templatePath);
   });
