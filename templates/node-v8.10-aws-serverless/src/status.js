e09235ccf9b6a88fc6f7ea43eaf26ff6f161fab1
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2019-03-24 01:41:30 +0100

    Big update of template node 8.10, handle async/await properly and lots of other things

--- templates/node-v8.10-aws-serverless/src/status.js
+++ templates/node-v8.10-aws-serverless/src/status.js
@@ -1,10 +1,20 @@
-export const handler = (event, context, callback) => {
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      running: 'OK',
-      NODE_ENV: process.env.NODE_ENV,
-      GIT_COMMIT: process.env.GIT_COMMIT_VERSION,
-    }),
-  });
-};
+import RavenLambdaWrapper from 'serverless-sentry-lib';
+import Raven from 'raven';
+import moment from 'moment';
+import { isHostedOnAws } from '@unly/utils-aws';
+
+export const handler = RavenLambdaWrapper.handler(Raven, async (event, context) => ({
+  statusCode: 200,
+  body: JSON.stringify({
+    status: 'OK',
+    environment: process.env.NODE_ENV,
+    service: process.env.SERVICE,
+    isHostedOnAws: isHostedOnAws(),
+    processNodeEnv: process.env.NODE_ENV,
+    time: moment().toISOString(),
+    release: process.env.GIT_COMMIT_VERSION,
+    branch: process.env.GIT_BRANCH,
+    releasedAt: process.env.DEPLOY_TIME,
+    version: process.env.npm_package_version,
+  }),
+}));
