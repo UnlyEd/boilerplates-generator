4db2f4b50b54bfde1461ef6608d244caa2d6363e
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2019-07-28 20:17:41 +0200

    Using "serverless-bundle": "1.2.3", fixed tests

--- templates/node-v10.x-aws-serverless-with-serverless-bundle/jest-preload-env.js
+++ templates/node-v10.x-aws-serverless-with-serverless-bundle/jest-preload-env.js
@@ -1,7 +1,5 @@
 require('dotenv').config({ path: '.serverless/.env' });
 
-console.log('env---', process.env.EXAMPLE_ENV_VAR_TEST_ONLY); // FIXME This is never called
-
 if (process.env.NODE_ENV !== 'test') {
   throw Error('Tests must be executed in a "test" environment (by convention)');
 }
