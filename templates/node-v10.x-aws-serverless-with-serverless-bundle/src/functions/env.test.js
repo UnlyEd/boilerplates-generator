9c5ff3c4c615599cfbbd78f05e2f3cc4230046c3
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2019-07-28 20:18:59 +0200

    No need to manually load env vars anymore

--- templates/node-v10.x-aws-serverless-with-serverless-bundle/src/functions/env.test.js
+++ templates/node-v10.x-aws-serverless-with-serverless-bundle/src/functions/env.test.js
@@ -1,8 +1,3 @@
-// We are forced to load env variables from each test file that needs it, because serverless-bundle doesn't properly handles "setupFilesAfterEnv"
-// XXX This forces us to use "require" instead of "import", because "import" are always loaded first and would therefore be loaded before the env is loaded
-//  (and may work, but may not, depending on the tests)
-require('dotenv').config({ path: '.serverless/.env' });
-
 describe('ENV', () => {
   test('should contain expected ENV var SERVICE', async () => {
     expect(process.env.SERVICE).toEqual('PROJECT-NAME');
